0314a9f7e1be358492b36bf831f1ace860fb8348
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2018-03-26 08:10:15 +0300

    refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,18 +8,18 @@ class HomeController < ApplicationController
     uploaded_io = params[:picture]
     dir = Rails.root.join('tmp')
     path = Rails.root.join('tmp', uploaded_io.original_filename)
+    tiff_path = Rails.root.join('tmp', 'text.tiff')
+    text_path = Rails.root.join('tmp', 'ara.txt')
     File.open(path, 'wb') do |file|
       file.write(uploaded_io.read)
-      tiff_path = "#{File.split(path).first}/text.tiff"
-      text_path = "#{dir}/ara.txt"
-      system("convert -depth 8 -density 300 #{path} #{tiff_path}")
-      system("./lib/textcleaner -g #{tiff_path} #{tiff_path}")
-      system("tesseract #{tiff_path} #{dir}/ara -l ara")
-      @extracted_text = File.read(text_path).to_s if File.exist?(text_path)
-      File.delete(path)
-      File.delete(tiff_path)
-      File.delete(text_path)
     end
+    system("convert -depth 8 -density 300 #{path} #{tiff_path}")
+    File.delete(path)
+    system("./lib/textcleaner -g #{tiff_path} #{tiff_path}")
+    system("tesseract #{tiff_path} #{dir}/ara -l ara")
+    File.delete(tiff_path)
+    @extracted_text = File.read(text_path).to_s if File.exist?(text_path)
+    File.delete(text_path)
     render :index
   end
 end
